ce1119dc9c9d85281bd24ba2a845f45d7decffb2
Laurentiu <laurentiucosminstanciu@yahoo.com>
2022-08-09 14:32:14 +0200

    initial code

---
+++ main.tf
@@ -0,0 +1,66 @@
+# 1. The version of the AzureRM Provider to use
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.0.1"
+    }
+  }
+}
+
+# 2. Configure the AzureRM Provider
+provider "azurerm" {
+  features {}
+}
+
+# 3. Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "demoRG"
+  location = "West Europe"
+}
+
+# 4. Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "demoVnet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# 5. Create a storage account
+resource "azurerm_storage_account" "example" {
+  name                     = "demosa01"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+# 6. create a Windows VM
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
